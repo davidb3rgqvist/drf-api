4fe9f8189fc5176c6905fff9bbe8856094093e38
David Bergqvist <David.bergqvist@gmail.com>
2024-09-30 14:58:09 +0000

    updated the post serializer because of fontend problem

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from posts.models import Post
 from likes.models import Like
 
-
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
@@ -16,13 +15,9 @@ class PostSerializer(serializers.ModelSerializer):
         if value.size > 2 * 1024 * 1024:
             raise serializers.ValidationError('Image size larger than 2MB!')
         if value.image.height > 4096:
-            raise serializers.ValidationError(
-                'Image height larger than 4096px!'
-            )
+            raise serializers.ValidationError('Image height larger than 4096px!')
         if value.image.width > 4096:
-            raise serializers.ValidationError(
-                'Image width larger than 4096px!'
-            )
+            raise serializers.ValidationError('Image width larger than 4096px!')
         return value
 
     def get_is_owner(self, obj):
@@ -32,11 +27,19 @@ class PostSerializer(serializers.ModelSerializer):
     def get_like_id(self, obj):
         user = self.context['request'].user
         if user.is_authenticated:
-            like = Like.objects.filter(
-                owner=user, post=obj
-            ).first()
+            like = Like.objects.filter(owner=user, post=obj).first()
             return like.id if like else None
         return None
+
+    def create(self, validated_data):
+        image = validated_data.pop('image', None)
+        post = Post.objects.create(**validated_data)
+
+        if image:
+            post.image = image
+            post.save()
+
+        return post
 
     class Meta:
         model = Post
