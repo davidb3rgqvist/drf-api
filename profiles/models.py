a173190e9ffcf9a4c2fe3864a1767c0c82c08357
David Bergqvist <David.bergqvist@gmail.com>
2024-10-01 15:22:50 +0000

    updated the default profilepicture

--- profiles/models.py
+++ profiles/models.py
@@ -11,7 +11,7 @@ class Profile(models.Model):
     content = models.TextField(blank=True)
     image = models.ImageField(
         upload_to='images/', 
-        default='https://res.cloudinary.com/dbar13vfu/image/upload/v1/media/images/default_profile_neleau.jpg'
+        default='../default_profile_mqli32'
     )
 
     class Meta:
